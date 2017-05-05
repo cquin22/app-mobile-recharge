48eac4428b7a0b95912a41b7352adbf1e10f3a2c
cristian-quintero-beitech <cristian.quintero@beitech.co>
2017-05-05 16:43:47 -0500

    Ajuste de configuraciones

--- api.mobile.recharges/api/controllers/RechargeController.js
+++ api.mobile.recharges/api/controllers/RechargeController.js
@@ -8,36 +8,11 @@
 const Promise = require("bluebird");
 
 
-var finOrCreateClient = function(data){
-  return new Promise(function(resolve, reject){
-    Client.findOrCreate(data , function(err, response){
-      if(err){
-        reject(err);
-      }else{
-        resolve(response);
-      }
-    });
-  });
-};
-
-var addRecharge = function(id, data){
-  return new Promise(function(resolve, reject){
-    Recharge.create({recharge_value: data.value, currency: data.currency, client: id} , function(err, response){
-      if(err){
-        reject(err);
-      }else{
-        resolve(response);
-      }
-    });
-  });
-};
-
-
 module.exports = {
 
   'create': function(req, res){
-    finOrCreateClient(req.body.client)
-      .then(function(client){ return addRecharge(client.id, req.body.recharge)})
+    this.finOrCreateClient(req.body.client)
+      .then(function(client){ return this.addRecharge(client.id, req.body.recharge)})
       .then(function(recharge){
         return res.AppResponse(200, 'UPDATE_SUCCESSFULLY', recharge);
       })
@@ -45,5 +20,29 @@ module.exports = {
         return res.AppResponse(400, 'USER_CREATED_SUCCESSFULLY', error);
       })
   },
+
+  'finOrCreateClient': function(data){
+    return new Promise(function(resolve, reject){
+      Client.findOrCreate(data , function(err, response){
+        if(err){
+          reject(err);
+        }else{
+          resolve(response);
+        }
+      });
+    });
+  },
+
+  'addRecharge': function (id, data) {
+    return new Promise(function(resolve, reject){
+      Recharge.create({recharge_value: data.value, currency: data.currency, client: id} , function(err, response){
+        if(err){
+          reject(err);
+        }else{
+          resolve(response);
+        }
+      });
+    });
+  }
 };
 
--- api.mobile.recharges/config/env/development.js
+++ api.mobile.recharges/config/env/development.js
@@ -16,9 +16,9 @@ module.exports = {
    * Set the default database connection for models in the development       *
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
-
-  // models: {
-  //   connection: 'someMongodbServer'
-  // }
+  hookTimeout: 40000,
+  models: {
+    connection: 'mongolabs'
+  }
 
 };
